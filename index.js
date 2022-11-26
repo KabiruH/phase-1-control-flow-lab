4f4e19f9a3ca0de28801983bafa2d8b64818c592
KabiruH <kelkab46@gmail.com>
2022-11-26 04:49:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet (rides){
   // Write your code here!
+  if (rides <= '400'){
+    return 'This one is on me!'
+  }
+  else if (rides>'2000', rides<'2500'){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (rides>'2500'){
+    return 'No can do.'
+  }
+ }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else if (city !== 'NYC'){
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+  }
+  if (tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  if (tip === 'thanks for everything'){
+    return 'Bye.'
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
